b262fa9610dbd00da280b3b54edb84be6031d7dc
Matej Prskalo <matejprskalo1234@gmail.com>
2022-09-29 10:18:13 +0200

    Init

---
+++ SPANTask/Helpers/ConvertHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+namespace SPANTask.Helpers
+{
+    public class ConvertHelper
+    {
+        public DataTable ConvertToDataTable<TSource>(IEnumerable<TSource> source)
+        {
+            var props = typeof(TSource).GetProperties();
+
+            var dt = new DataTable();
+            dt.Columns.AddRange(
+              props.Select(p => new DataColumn(p.Name, p.PropertyType)).ToArray()
+            );
+
+            source.ToList().ForEach(
+              i => dt.Rows.Add(props.Select(p => p.GetValue(i, null)).ToArray())
+            );
+
+            return dt;
+        }
+    }
+}
